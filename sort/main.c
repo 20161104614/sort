d02564bb88ebdf6ef1ed340fc72ee8a8c7096df4
20161104614 <a20161104614@scy.local>
2017-06-12 11:36:47 +0800

    sort
    Sort of numbers

--- sort/main.c
+++ sort/main.c
@@ -7,9 +7,29 @@
 //
 
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main()
+{
+    int i,a[10],j,t;
+    for(i=0;i<10;i++)
+    {
+        scanf ("%d",&a[i]);
+    }
+    for(j=0;j<9;j++)
+    {
+    for(i=0;i<=8-j;j++)
+    {
+        if(a[i]>a[i+1])
+        {
+            t=a[i];
+            a[i]=a[i+j];
+            a[i+1]=t;
+        }
+    }
+    }
+    for(i=0;i<10;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    printf("\n");
     return 0;
 }
